14a7de7d3eac8356d83cb70b6c6e34042281a546
Atheer83 <atheer.for.tech@gmail.com>
2018-03-01 14:43:18 +0200

    second version :2:45 PM

--- main.js
+++ main.js
@@ -1,36 +1,38 @@
-	var studentsInfo = [];
 
 $(function() {
-$("body").css("background-color","lightblue")
-$("h1").css("text-align", "center")
+$("body").css("background-color","white")
 $(".sImage").css({
-	"height" : "300px",
+	"height" : "500px",
     "width": "100%"})
 $('.slideShow').css({
-	"margin": "0 auto",
-    "width": "55%",
-    "border": "3px solid green",
-    "padding": "10px"});
-
-$('.gallary').css({
-	"margin": " auto",
-    "width": "95%",
+	"margin": "auto",
+	"margin-top":"100px",
+	"margin-bottom":"70px",
+    "width": "65%",
+    "border": "3px solid #4CAF50",
     "padding": "10px"});
 
 
+//local storge
+var studentsInfos = localStorage.getItem("studentsInfo");
+studentsInfos = JSON.parse(studentsInfos);
+//slide show and students array info
 var gallaryImage = $(".slideShow").find("img").first();
-
+var newArr = [];
+var arrName =[];
+var arrfWhere=[];
+var arrAge =[];
+var arrWork =[];
 var image= [
-		"images/student1.jpg",
-		"images/student2.jpg",
-		"images/student3.jpg",
-		"images/student4.jpg",
-		"images/student5.jpg",
-		"images/student6.jpg"
+		"slideshow/slide1.jpg",
+		"slideshow/slide2.jpg",
+		"slideshow/slide3.jpg",
+		"slideshow/slide4.jpg",
+		"slideshow/slide5.jpg",
+		"slideshow/slide6.jpg"
 		];
 
 	var i =0;
-	
 	setInterval(function(){
 		i = (i+1)%image.length;
 		gallaryImage.fadeOut(function(){
@@ -38,9 +40,61 @@ var image= [
 			$(this).fadeIn();
 		});
 	},3000);
+// Factory functions
+function makeGallary(array) {
+	
+	for (var i =0; i< array.length; i++) {
+		newArr.push(array[i].photo);
+		$('.gallary').append("<div id='fix'><img src='" + array[i].photo + "'/></div>");
+	}
+	return newArr;
+}
 
+function makeName(array) {
+	
+	for (var i =0; i< array.length; i++) {
+		arrName.push(array[i].name);
+		$('.gallary img').eq(i).after("<div><strong>" + array[i].name + "</strong><br></div> ");
+	}
+	return arrName;
+}
+function makefWhere(array) {
+	
+	for (var i =0; i< array.length; i++) {
+		arrfWhere.push(array[i].fWhere);
+		$('.gallary #fix div').eq(i).append("<em>" + array[i].fWhere + "</em><br>");
+	}
+	return arrfWhere;
+}
+function makeAge(array) {
+	
+	for (var i =0; i< array.length; i++) {
+		arrAge.push(array[i].age);
+		$('.gallary #fix div').eq(i).append("<b>" + array[i].age + "</b><br>");
+	}
+	return arrfWhere;
+}
+function makeWork(array) {
+	
+	for (var i =0; i< array.length; i++) {
+		arrWork.push(array[i].work);
+		$('.gallary #fix div').eq(i).append("<b>" + array[i].work + "</b>");
+	}
+	return arrfWhere;
+}
+
+makeGallary(studentsInfos);
+makeName(studentsInfos);
+makefWhere(studentsInfos);
+makeAge(studentsInfos);
+makeWork(studentsInfos);
+
+
+
+//photo profile
 var gallaryItems = $(".gallary").find("img");
-	gallaryItems.css("width","24%").css("opacity","0.7");
+	gallaryItems.css("width", "100%")
+
 	gallaryItems.mouseenter(function(){
 		$(this).stop().fadeTo("500","1")
 		
@@ -52,10 +106,9 @@ var gallaryItems = $(".gallary").find("img");
 
 	gallaryItems.click(function(){ 
 		var source = $(this).attr("src");
-		var imageI = $("<img>").attr("src", source).css("width","100%");
+		var imageI = $("<img>").attr("src", source).css("width","100%").css("border-radius","50%");
 		
 		$(".lightbox").empty().append(imageI).fadeIn(2000);
-		$(imageI).css({paddingTop: "20px"})
 	});
 
 		$(".lightbox").click(function(){ 
@@ -64,57 +117,44 @@ var gallaryItems = $(".gallary").find("img");
 	});
 
 
-	//========= students.html ==========/
+	//========= students Alumni ==========/
 	
+	var studentsInfo = [];
 
 	$("#addStudent").click(function() {
 	var name = $("#name").val();
 	var age = $("#age").val();
 	var fWhere = $("#fWhere").val();
 	var work = $("#work").val();
-	var index = $("#index").val()
-	var idx = Number (index)
+	var index = $("#index").val();
+	var photo = "images/" + $('input[type=file]').val().split('\\').pop();
+	console.log(photo)
 
-	function addInfo(name, age, fWhere, work) {
+	var idx = Number (index);
+
+	function addInfo(name, age, fWhere, work, photo) {
 		return {
 			name: name,
 			age: age,
 			fWhere: fWhere,
 			work: work,
+			photo: photo,
 		}
 	
 	};
-		console.log(idx)
 
 	var student = "student" + idx;
-		console.log(student)
 
-		student =addInfo(name, age, fWhere, work)
-		console.log(student)
+		student =addInfo(name, age, fWhere, work, photo)
 		studentsInfo[idx - 1] =student;
-		console.log(studentsInfo)
+		localStorage.setItem("studentsInfo", JSON.stringify(studentsInfo));
 	
 	$("#info").find("input:text").val('');
 		idx = idx + 1 ;
 		index = $("#index").val(idx);
 	});
 
-
-
 });
-
-// 	document.getElementById('getval').addEventListener('change', readURL, true);
-// 	function readURL(){
-// 	   var file = document.getElementById("getval").files[0];
-// 	   var reader = new FileReader();
-//    reader.onloadend = function(){
-//       document.getElementById('clock').style.backgroundImage = "url(" + reader.result + ")";        
-//    }
-//    if(file){
-//       reader.readAsDataURL(file);
-//     }else{
-//     }
-// }
 
 
 
